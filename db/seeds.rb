793708a7101bb74d4930631b77f523d4f88e29ac
gmwaka <warrengrace08@gmail.com>
2021-05-08 02:07:43 -0400

    feat: Added load initial data script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('db', 'data', 'pokemon.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Pokemon.new
+  t.name = row['Name']
+  t.type1 = row['Type 1']
+  t.type2 = row['Type 2']
+  t.total = row['Total']
+  t.hp = row['HP']
+  t.attack = row['Attack']
+  t.defense = row['Defense']
+  t.spatk = row['Sp. Atk']
+  t.spdef = row['Sp. Def']
+  t.speed = row['Speed']
+  t.generation = row['Generation']
+  t.legendary = row['Legendary']
+  t.save
+  puts "#{t.name}, #{t.total} saved"
+end
+
+puts "There are now #{Pokemon.count} rows in the pokemons table"
